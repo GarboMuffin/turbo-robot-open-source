a990554179542836214ed465a7e8d00a34190f80
Muffin <muffin@muffin.ink>
2024-05-08 13:24:33 -0500

    Update example config

--- config.example.js
+++ config.example.js
@@ -3,26 +3,26 @@
 module.exports = {
     // Create an application on https://discord.com/developers/applications then
     // copy and paste the application ID here as a string.
-    applicationId: '',
+    applicationId: '1234',
 
     // On https://discord.com/developers/applications enable bot for the application,
     // then copy and paste the bot token here.
-    token: '',
+    token: 'abc.def.ghi',
 
     // Create a role for mods (does not need any actual permissions), then
     // copy and paste its ID here as a string.
-    modRoleId: '',
+    modRoleId: 'right click on the role > copy id',
 
     // Create a channel for mod stuff (its where /pingmods goes), then
     // copy and paste its ID here as a string.
-    modChannelId: '',
+    modChannelId: 'right click on the channel > copy id',
 
     // Create a channel for the starboard, then
     // copy and paste its ID here as a string.
     starboardChannelId: 'right click on the channel > copy id',
 
-    // Path to where the database is saved. Leave it empty to save it in the
-    // same folder as the source code, which is fine for development (it is gitignored).
-    // In production see mount-encrypted-storage.example.sh.
+    // Path to folder to save database in. Leave it empty to save it in the same
+    // folder as the source code, which is fine for development (it is gitignored).
+    // In production we recommend reading mount-encrypted-storage.example.sh.
     dataPath: ''
 };
