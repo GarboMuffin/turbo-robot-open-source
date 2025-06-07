8900eb1482306a42a79590a7f95925e7890f0117
Mov <man.o.valor.train@gmail.com>
2025-06-06 20:42:15 -0400

    Actually respond to the interactions
    im so smart
    
    also it wont timeout moderators now

--- src/modules/timeout.js
+++ src/modules/timeout.js
@@ -1,4 +1,4 @@
-const { MessageFlags } = require('discord.js');
+const { MessageFlags, PermissionsBitField } = require('discord.js');
 
 const timeout = async (interaction) => {
   const user = interaction.options.getUser('user');
@@ -7,11 +7,22 @@ const timeout = async (interaction) => {
   const reason = interaction.options.getReason('reason') ?? "No reason provided";
 
   try {
-    await member.timeout(amount*60000, reason);
-    await user.send({
-      content: `⏲️ You have been timed out in the Turbowarp server for the following reason: ${reason}`,
-      flags: MessageFlags.SuppressNotifications
-    });
+    if (member.permissions.has(PermissionsBitField.Flags.ModerateMembers)) {
+      await member.timeout(amount*60000, reason);
+      await user.send({
+        content: `⏲️ You have been timed out in the Turbowarp server for the following reason: ${reason}`,
+        flags: MessageFlags.SuppressNotifications
+      });
+      await interaction.reply({
+        content: `⏲️ Successfully timed out <@${user.id}> for ${amount} minutes with reason: ${reason}`,
+        flags: MessageFlags.Ephemeral
+      })
+    } else {
+      await interaction.reply({
+        content: `Failed to timeout user: You can't timeout moderators!`,
+        flags: MessageFlags.Ephemeral
+      })
+    }
   } catch (error) {
     await interaction.reply({
       content: 'Failed to timeout user.',
