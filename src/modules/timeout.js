172d868600a8332309115bc901680794fd0abee5
Mov <man.o.valor.train@gmail.com>
2025-06-06 21:18:49 -0400

    Forgot an exclamation point

--- src/modules/timeout.js
+++ src/modules/timeout.js
@@ -7,7 +7,7 @@ const timeout = async (interaction) => {
   const reason = interaction.options.getString('reason') ?? "No reason provided";
 
   try {
-    if (member.permissions.has(PermissionsBitField.Flags.ModerateMembers)) {
+    if (!member.permissions.has(PermissionsBitField.Flags.ModerateMembers)) {
       await member.timeout(amount*60000, reason);
       await user.send({
         content: `⏲️ You have been timed out in the Turbowarp server for the following reason: ${reason}`,
