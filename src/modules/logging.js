111c086877fa3c530ea3961cc712e7c86de08521
Thomas Weber <muffin@muffin.ink>
2026-08-22 02:00:16 -0500

    Revert "Handle webhook errors w/o unhandled rejection"
    This reverts commit 7de75dcbd1bb74aea2e27de75873e8083891314c.

--- src/modules/logging.js
+++ src/modules/logging.js
@@ -461,20 +461,15 @@ const auditLogs = async (auditLog) => {
   }
 
   if (webhookLog && config.majorOffensesSignalingService) {
-    try {
-      await fetch(config.majorOffensesSignalingService, {
-        method: 'POST',
-        signal: AbortSignal.timeout(10000),
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify({
-          content: webhookLog
-        })
-      });
-    } catch (error) {
-      console.error('Failed to send webhook:', error);
-    }
+    await fetch(config.majorOffensesSignalingService, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        content: webhookLog
+      })
+    });
   }
 };
 
